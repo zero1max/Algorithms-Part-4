918bd8dd597222deb2798e7be0099e53d6988bd7
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-11-26 10:09:35 +0500

    Update Commit

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 
 <h2>Tasks</h2>
 
-<img src="img/img1.jpg" alt="img1.jpg">
+<img src="img/img7891011.jpg" alt="img1.jpg">
 
 <h2>Task 1</h2>
 
-<img src="img/img2.png" alt="img2.png">
+<img src="img/img1.png" alt="img2.png">
 
 <h2>Task 2</h2>
+
+<img src="img/img2.png" alt="img3.png">
+
+<h2>Task 3</h2>
 
 <img src="img/img3.png" alt="img3.png">
---
+++ algorithms/task3.py
@@ -0,0 +1,21 @@
+# -------------- Task 1 ---------------------
+
+# def age(user_age: int):
+#     """User tug'ilgan yilini kiriting"""
+#     return 2024-user_age
+
+# print(age(2008))
+
+# -------------- Task 2 ------------------------
+
+# def soz(a: str):
+#     return a[::-1]
+
+# print(soz('Hello'))
+
+# -------------- Task 3 ----------------------
+
+def kvadrat(b: int):
+    return b**2
+
+print(kvadrat(2))
